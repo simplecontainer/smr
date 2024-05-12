04276a5e9b6279047d9145acd9fe42240761f1b1
qdnqn <adnn.selimovic@gmail.com>
2024-05-12 14:19:46 +0200

    Fix cleanup

--- pkg/static/resources/shell/mysql_import.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-DATABASE=$1
-
-echo "ghostmgr: Importing sql file from the previous database."
-export MYSQL_PWD=$(echo $MYSQL_ROOT_PASSWORD)
-mysql -u root $DATABASE < /tmp/ghost.sql
