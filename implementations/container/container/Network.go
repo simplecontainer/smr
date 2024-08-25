7904e2884cc2d4ca62a9f1a77a305e9b083156e4
qdnqn <adnn.selimovic@gmail.com>
2024-08-25 18:44:44 +0200

    Update DNS on restore

--- implementations/container/container/Network.go
+++ implementations/container/container/Network.go
@@ -38,6 +38,34 @@ func (container *Container) GetNetworkInfoTS() map[string]Network {
 	return networkCopy
 }
 
+func (container *Container) ConnectToNetwork(containerId string, networkId string) error {
+	if c, _ := container.Get(); c != nil && c.State == "running" {
+		ctx := context.Background()
+		cli, err := client.NewClientWithOpts(client.FromEnv, client.WithAPIVersionNegotiation())
+		if err != nil {
+			panic(err)
+		}
+		defer cli.Close()
+
+		// TODO: Don't connect if the network is same
+
+		EndpointSettings := &network.EndpointSettings{
+			NetworkID: networkId,
+		}
+
+		err = cli.NetworkConnect(ctx, networkId, containerId, EndpointSettings)
+
+		if err != nil {
+			logger.Log.Error(err.Error())
+			return errors.New("failed to connect to the network")
+		}
+
+		return nil
+	} else {
+		return errors.New("container is not running")
+	}
+}
+
 func (container *Container) CreateNetwork() error {
 	if !container.FindNetwork() {
 		cli, err := client.NewClientWithOpts(client.FromEnv)
