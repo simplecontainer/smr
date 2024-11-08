1431830faebf6ff5781f56090f0d45496a8703c0
qdnqn <adnn.selimovic@gmail.com>
2024-11-08 12:47:46 +0100

    Remove comment and upgrade to 0.0.52 for correct go resolving of modules

--- main.go
+++ main.go
@@ -15,25 +15,6 @@ import (
 	"os"
 )
 
-//	@title			Simple container manager API
-//	@version		1.0
-//	@description	This is a container orchestrator service.
-//	@termsOfService	http://smr.qdnqn.com/terms
-
-//	@contact.name	API Support
-//	@contact.url	https://github.com/simplecontainer/smr
-
-//	@license.name	GNU General Public License v3.0
-//	@license.url	https://github.com/simplecontainer/smr/blob/main/LICENSE
-
-//	@host		localhost:8080
-//	@BasePath	/api/v1
-
-//	@securityDefinitions.basic	BasicAuth
-
-//	@externalDocs.description	OpenAPI
-//	@externalDocs.url			https://swagger.io/resources/open-api/
-
 func main() {
 	startup.SetFlags()
 	logger.Log = logger.NewLogger()
