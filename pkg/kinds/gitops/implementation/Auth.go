f87dadbf6aadffecd45ee9213ebed9fe89935083
qdnqn <adnn.selimovic@gmail.com>
2025-01-04 19:12:15 +0100

    Fix: handle gracefuly gitops failure to resolve reference

--- pkg/kinds/gitops/implementation/Auth.go
+++ pkg/kinds/gitops/implementation/Auth.go
@@ -1,12 +1,10 @@
 package implementation
 
 import (
-	"crypto/x509"
 	"encoding/base64"
 	"github.com/go-git/go-git/v5/plumbing/transport"
 	gitHttp "github.com/go-git/go-git/v5/plumbing/transport/http"
 	"github.com/go-git/go-git/v5/plumbing/transport/ssh"
-	"github.com/simplecontainer/smr/pkg/keys"
 )
 
 func (gitops *Gitops) GetAuth() (transport.AuthMethod, error) {
@@ -22,13 +20,6 @@ func (gitops *Gitops) GetAuth() (transport.AuthMethod, error) {
 
 	if gitops.AuthInternal.CertKey != nil {
 		b64decoded, _ := base64.StdEncoding.DecodeString(gitops.AuthInternal.CertKey.PrivateKey)
-
-		_, err = x509.ParsePKCS8PrivateKey(keys.PEMDecode(b64decoded))
-
-		if err != nil {
-			return nil, err
-		}
-
 		auth, err = ssh.NewPublicKeys(ssh.DefaultUsername, b64decoded, gitops.AuthInternal.CertKey.PrivateKeyPassword)
 	}
 
