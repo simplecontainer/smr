fc30cc9e81a415fbcacad874537c5fa989968dcc
qdnqn <adnn.selimovic@gmail.com>
2025-06-21 13:34:40 +0200

    Errand: Reuse platform Clean in node clean

--- pkg/engine/node/Clean.go
+++ pkg/engine/node/Clean.go
@@ -3,7 +3,6 @@ package node
 import (
 	"fmt"
 	"github.com/simplecontainer/smr/internal/helpers"
-	"github.com/simplecontainer/smr/pkg/static"
 )
 
 func Clean() {
@@ -13,23 +12,11 @@ func Clean() {
 		helpers.PrintAndExit(err, 1)
 	}
 
-	defer func() {
-		err = container.Delete()
-
-		if err != nil {
-			helpers.PrintAndExit(err, 1)
-		}
-
-		fmt.Println("node deleted")
-	}()
-
-	err = container.Stop(static.SIGTERM)
+	err = container.Clean()
 
 	if err != nil {
 		helpers.PrintAndExit(err, 1)
 	}
 
-	err = container.Wait("removed")
-
-	fmt.Println("node stopped")
+	fmt.Println("node container removed")
 }
