9f497d9e426b7570599403e9b3b5e583f1b947fa
qdnqn <adnn.selimovic@gmail.com>
2025-07-21 15:16:29 +0200

    Fix: Refactor contexts

--- pkg/tests/flags/Flags.go
+++ pkg/tests/flags/Flags.go
@@ -19,7 +19,7 @@ func init() {
 	flag.StringVar(&Home, "root", helpers.GetRealHome(), "Root directory of all file I/O - should be the home of the user")
 	flag.StringVar(&BinaryPath, "binary", "smr-linux-amd64/smr", "Path to where smr binary to use - path needs to have smr binary with same name")
 	flag.StringVar(&BinaryPathCli, "binaryctl", "smrctl-linux-amd64/smrctl", "Path to where smr binary to use - path needs to have smr binary with same name")
-	flag.StringVar(&ExamplesDir, "examples", "../examples", "Path to where smr binary to use - path needs to have smr binary with same name")
+	flag.StringVar(&ExamplesDir, "examples", "../definitions/examples", "Path to where smr binary to use - path needs to have smr binary with same name")
 	flag.StringVar(&Image, "image", "smr", "SMR image name to use")
 	flag.StringVar(&Tag, "tag", "latest", "SMR image tag to use")
 	flag.IntVar(&Timeout, "timeout", 180, "Timeout in seconds for operations")
