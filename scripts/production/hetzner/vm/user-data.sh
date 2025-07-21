9c946059c2eba42d5934005bf4e7c238115e369b
qdnqn <adnn.selimovic@gmail.com>
2025-07-21 19:21:24 +0200

    Errand: Adapt hetzner vm user_data script

--- scripts/production/hetzner/vm/user-data.sh
+++ scripts/production/hetzner/vm/user-data.sh
@@ -30,6 +30,8 @@ runcmd:
   - apt-get install -y docker-ce docker-ce-cli containerd.io
   - systemctl enable docker
   - systemctl start docker
+  - mkdir -p /home/node/.ssh
+  - cp /root/.ssh/authorized_keys /home/node/.ssh/authorized_keys && chown -R node /home/node/.ssh
 
   # Install SMR and set up system
   - su - node -c "curl -sL https://raw.githubusercontent.com/simplecontainer/smr/refs/heads/main/scripts/production/smrmgr.sh -o smrmgr"
@@ -38,11 +40,10 @@ runcmd:
   - su - node -c "sudo smrmgr install"
 
   # Get Hetzner metadata
-  - export TOKEN={{ .token }}
-  - export ACTION={{ .action }}
-  - LOCAL_IP=$(curl -s http://169.254.169.254/metadata/local-ipv4)
-  - PUBLIC_HOSTNAME=$(curl -s http://169.254.169.254/metadata/hostname)
-  - su - node -c "smrmgr start -a $LOCAL_IP -d $PUBLIC_HOSTNAME -s"
+  - PUBLIC_IP=$(curl -s http://169.254.169.254/hetzner/v1/metadata/public-ipv4 | sed 's#/.*##')
+  - PUBLIC_HOSTNAME=$(curl -s http://169.254.169.254/hetzner/v1/metadata/instance-id).nodes.simplecontainer.io
+  - su - node -c "smrmgr start -a $PUBLIC_IP -d $PUBLIC_HOSTNAME -s"
   - su - node -c "smrmgr service-install"
-  - su - node -c "sudo systemctl set-environment TOKEN="$TOKEN" ACTION="$ACTION"
+  - su - node -c "sudo systemctl set-environment TOKEN_ENV='{{ .token }}'"
+  - su - node -c "sudo systemctl set-environment ACTION_ENV='{{ .action }}'"
   - su - node -c "sudo systemctl start simplecontainer@node"
